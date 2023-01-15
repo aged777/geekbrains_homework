9661bbde2b5707d537739ee114cc26e743817326
Aleksandr Volkov <222volkov@gmail.com>
2023-01-15 18:21:44 +0300

    task2 finished

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Задача 2: 
+//  Напишите программу, которая на вход принимает два числа и выдаёт, 
+//  какое число большее, а какое меньшее.
+
+//  a = 5; b = 7 -> max = 7
+//  a = 2 b = 10 -> max = 10
+//  a = -9 b = -3 -> max = -3
+
+// В задаче не указано, какие должны быть числа, мы должны это уточнить, 
+// но так как в примерах указаны только целые числа, работаем с ними, но в обращении к
+// пользователю укажем "Введите 2  целых числа" 
+
+Console.WriteLine("Введите 2 целых числа: ");
+
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+
+if(num1 < num2) Console.WriteLine("max = " + num2);
+else if(num1 > num2) Console.WriteLine("max = " + num1);
+else Console.WriteLine("Числа равны.");
